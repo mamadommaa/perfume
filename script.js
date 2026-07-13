f55741d4ac5ecb47cd678ea164a8701622e5d713
Mac <mac@MacBook-Pro-Mac.local>
2026-07-13 20:34:20 +0300

    13 july

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const preview = document.querySelector('.preview');
 const block = document.querySelector('.something-block');
 const video = document.querySelector('video');
 
+// добавляем музычку
+const audio = document.querySelector('.audio');
+const btn = document.querySelector('.header__music');
+
 tie.addEventListener('click', () => {
   setTimeout(() => {
     preview.style.display = 'none';
@@ -11,6 +15,18 @@ tie.addEventListener('click', () => {
     block.classList.add('show');
 
     video.play();
+
+    audio.currentTime = 18;
+    audio.play();
   }, 600);
+});
+
+btn.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+  btn.classList.toggle('muted');
 });
 
